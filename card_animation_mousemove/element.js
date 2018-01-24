fd063376cb6a3ffe5c30be19262e6eb27ef4bc22
Rodion <rodionvolovik@gmail.com>
2018-01-24 18:54:42 +0200

    Add files via upload

---
+++ card_animation_mousemove/element.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	element = $(".element-item");
+	width = element.width();
+	height = element.height();
+	offset = element.offset();
+	x_offset = offset.left;
+	y_offset = offset.top;
+	element.mousemove(function (e) {
+		var x = e.clientX;
+		var y = e.clientY;
+		posX = x - x_offset;
+		posY = y - y_offset;
+		angleX = posX / width * 20 - 10;
+		angleY = posY / height * 20 - 10;
+		element.css('transform', 'perspective(1500px) rotateX('+angleX+'deg) rotateY('+angleY+'deg) scale3d(1,1,1)');
+		console.log(-1 * angleX);
+		console.log(angleY);
+	});
+	element.mouseleave(function (e) {
+		element.css('transform', 'perspective(1500px) rotateX(0deg) rotateY(0deg)');
+	});
+});
