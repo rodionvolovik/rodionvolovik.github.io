bb6b01e92b78340adef24b02ce43077b86ede289
rodionvolovik <rodionvolovik@gmail.com>
2018-01-29 09:09:49 +0200

    dfsdf

--- js/header.js
+++
@@ -1,8 +0,0 @@
-$(document).scroll(function() {
-	element = $(".nav-wrapper");
-	if ($(document).scrollTop() >= 50) {
-		$(element).addClass("shadow");
-	} else {
-		$(element).removeClass("shadow");
-	}
- });
